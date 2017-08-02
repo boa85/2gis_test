b37a463df667c4d0b71aa2945426468b8f9e0103
boa <boa@promobit.ru>
2017-08-02 21:22:40 +0600

    added basic interface into command_handler class and file_reader class

--- command_handler/include/command_handler.hpp
+++ command_handler/include/command_handler.hpp
@@ -5,10 +5,30 @@
 #ifndef WORD_COUNTER_COMMAND_HANDLER_HPP
 #define WORD_COUNTER_COMMAND_HANDLER_HPP
 
+#include <string>
+#include <memory>
+#include "file_reader.hpp"
 
-class command_handler {
+namespace word_counter {
+    using namespace file_reader;
+    namespace command_handler {
 
-};
+        class CommandHandler {
+        public:
+            CommandHandler();
+
+            virtual ~CommandHandler() = default;
+
+            void countWords(const std::string &filename, const std::string word);
+
+            void calculateChecksum(const std::string &filename);
+
+        private:
+            std::unique_ptr<FileReader> fileReader_;
+        };//class CommandHandler
+    }//namespace command_handler
+}//namespace word_counter
+
 
 
 #endif //WORD_COUNTER_COMMAND_HANDLER_HPP
