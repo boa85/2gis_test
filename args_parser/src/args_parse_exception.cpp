767d1c8aa707a138681253efedebdf6fdb1c4037
boa <boa@promobit.ru>
2017-08-04 23:05:52 +0600

    fixed what method

--- args_parser/src/args_parse_exception.cpp
+++ args_parser/src/args_parse_exception.cpp
@@ -2,15 +2,17 @@
 // Created by boa on 04.08.17.
 //
 
+#include <cstring>
+#include <memory>
 #include "../include/args_parse_exception.hpp"
 
 namespace word_counter {
     namespace argument_parser {
-        ArgsParseException::ArgsParseException(const std::string &message) throw()
+        ArgsParseException::ArgsParseException(const std::string &message)  throw()
                 : errorMessage_(message) {}
 
         const char *ArgsParseException::what() const throw() {
-            return std::string("argument parser " + errorMessage_).c_str();
+            return strcpy(new char[errorMessage_.size()], errorMessage_.c_str());//FIXME memory leak
         }
     }
 }
