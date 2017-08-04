181207238bc472ccee065636d1de24cf7c4d5d21
boa <boa@promobit.ru>
2017-08-04 23:57:04 +0600

    added simple implementation the ICommand interface for WordCountCommand class;

---
+++ command_handler/include/word_count_command.hpp
@@ -0,0 +1,25 @@
+//
+// Created by boa on 04.08.17.
+//
+
+#ifndef WORD_COUNTER_WORD_COUNT_COMMAND_HPP
+#define WORD_COUNTER_WORD_COUNT_COMMAND_HPP
+
+#include <string>
+
+#include "icommand.hpp"
+
+namespace word_counter {
+    namespace command_handler {
+        class WordCountCommand : public ICommand {
+        public:
+            WordCountCommand(const std::string &filename, const std::string &searchWord);
+
+            void execute() override;
+        };
+
+    }
+}
+
+
+#endif //WORD_COUNTER_WORD_COUNT_COMMAND_HPP
---
+++ command_handler/src/word_count_command.cpp
@@ -0,0 +1,18 @@
+//
+// Created by boa on 04.08.17.
+//
+
+#include "../include/word_count_command.hpp"
+
+namespace word_counter {
+    namespace command_handler {
+        void WordCountCommand::execute() {
+
+        }
+
+        WordCountCommand::WordCountCommand(const std::string &filename, const std::string &searchWord) {
+
+        }
+    }
+}
+
