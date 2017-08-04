81df523894949f14d37bac311e6948fbd75711d7
boa <boa@promobit.ru>
2017-08-04 06:25:29 +0600

    removed old implementation a CommandHandler class removed FileReader class

--- command_handler/include/command_handler.hpp
+++ command_handler/include/command_handler.hpp
@@ -7,32 +7,14 @@
 
 #include <string>
 #include <functional>
-#include <bits/unique_ptr.h>
-#include "../../service/include/service.hpp"
-#include "file_reader.hpp"
 
 namespace word_counter {
-    using namespace service;
-    using namespace file_reader;
     namespace command_handler {
         class CommandHandler {
         public:
             explicit CommandHandler();
 
-            virtual ~CommandHandler() = default;
 
-            void runCountWordsMode(const std::string &filename, std::string &searchWord);
-
-            void runCalculateChecksumMode(const std::string &filename);
-
-        private:
-            std::unique_ptr<FileReader> fileReader_;
-
-            void countWords(const Text &text);
-
-            void calculateChecksum(const Text &text);
-
-            std::string searchWord_;
         };//class CommandHandler
 
     }//namespace command_handler
--- command_handler/src/command_handler.cpp
+++ command_handler/src/command_handler.cpp
@@ -2,34 +2,8 @@
 // Created by boa on 02.08.17.
 //
 
-#include "../include/command_handler.hpp"
-#include <iostream>
-
 namespace word_counter {
     namespace command_handler {
-        CommandHandler::CommandHandler() : fileReader_(std::make_unique<FileReader>()) {
-
-        }
-
-        void CommandHandler::runCountWordsMode(const std::string &filename, std::string &searchWord) {
-            searchWord_ = std::move(searchWord);
-            fileReader_->getText.connect(boost::bind(&CommandHandler::countWords, this, _1));
-            fileReader_->read(filename);
-        }
-
-        void CommandHandler::runCalculateChecksumMode(const std::string &filename) {
-            fileReader_->getText.connect(boost::bind(&CommandHandler::calculateChecksum, this, _1));
-            fileReader_->read(filename);
-            std::cout << "filename = " << filename << std::endl;
-        }
-
-        void CommandHandler::countWords(const Text &text) {
-
-        }
-
-        void CommandHandler::calculateChecksum(const Text &text) {
-
-        }
 
     }//namespace command_handler
 }//namespace word_counter
