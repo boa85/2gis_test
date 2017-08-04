878e64ef1ab2b38db065ebfd4e732a5787aad2fe
boa <boa@promobit.ru>
2017-08-04 23:53:18 +0600

    include header

--- command_handler/src/command_handler.cpp
+++ command_handler/src/command_handler.cpp
@@ -2,8 +2,11 @@
 // Created by boa on 02.08.17.
 //
 
+#include "../include/command_handler.hpp"
+
 namespace word_counter {
     namespace command_handler {
+
 
     }//namespace command_handler
 }//namespace word_counter
