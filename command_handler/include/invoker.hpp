5b2dbb9498c0d9c6112aff88f9721b27c41f9469
boa <boa@promobit.ru>
2017-08-05 20:57:51 +0600

    Added Invoker class for creating commands and transferring them to the executor

---
+++ command_handler/include/invoker.hpp
@@ -0,0 +1,39 @@
+//
+// Created by boa on 05.08.17.
+//
+
+#ifndef WORD_COUNTER_INVOKER_HPP
+#define WORD_COUNTER_INVOKER_HPP
+
+#include <memory>
+
+#include <boost/signals2.hpp>
+
+#include "icommand.hpp"
+
+namespace word_counter {
+    namespace command_handler {
+        class Invoker {
+        public:
+            Invoker() = default;
+
+            virtual  ~Invoker() = default;
+
+            void countNumberOccurrencesWord(std::string &filename, std::string &searchWord);
+
+            void calculateCheckSum(std::string &filename);
+
+            boost::signals2::signal<void(std::shared_ptr<ICommand>)> start;
+        protected:
+            std::shared_ptr<ICommand> createCountOccurrencesWordCommand(std::string &filename, std::string &searchWord);
+
+            std::shared_ptr<ICommand> createChecksumCommand(std::string &filename);
+
+        };
+
+    }//namespace command_handler
+}//namespace word_counter
+
+
+
+#endif //WORD_COUNTER_INVOKER_HPP
---
+++ command_handler/src/invoker.cpp
@@ -0,0 +1,31 @@
+//
+// Created by boa on 05.08.17.
+//
+
+#include "../include/invoker.hpp"
+#include "../include/word_count_command.hpp"
+#include "../include/checksum_command.hpp"
+
+namespace word_counter {
+    namespace command_handler {
+        std::shared_ptr<ICommand> Invoker::createCountOccurrencesWordCommand(std::string &filename,
+                                                                             std::string &searchWord) {
+            return std::make_shared<CountOccurrencesWordCommand>(filename, searchWord);
+        }
+
+        std::shared_ptr<ICommand> Invoker::createChecksumCommand(std::string &filename) {
+            return std::make_shared<ChecksumCommand>(filename);
+        }
+
+        void Invoker::countNumberOccurrencesWord(std::string &filename, std::string &searchWord) {
+            auto command = createCountOccurrencesWordCommand(filename, searchWord);
+            start(command);
+        }
+
+        void Invoker::calculateCheckSum(std::string &filename) {
+            auto command = createChecksumCommand(filename);
+            start(command);
+        }
+
+    }//namespace command_handler
+}//namespace word_counter
