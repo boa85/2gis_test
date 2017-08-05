9fb1962c180fd94a6db7552931bebdcc1b3b75ff
boa <boa@promobit.ru>
2017-08-05 21:39:28 +0600

    почти готовый интерфейс, осталось дописать реализацию методов execute в командах

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "args_parser/include/argument_parser.hpp"
 #include "command_handler/include/command_handler.hpp"
 #include "args_parser/include/args_parse_exception.hpp"
+#include "command_handler/include/invoker.hpp"
 /**
  * Напишите (консольную) программу, принимающую на вход  имя файла и набор параметров.
  * В зависимости от параметров программа должна работать в трёх режимах:
@@ -20,7 +21,11 @@ using namespace command_handler;
 int main(int argc, char *argv[]) {
     try {
         auto parser = std::make_shared<ArgumentParser>();
+        auto invoker = std::make_shared<Invoker>();
         auto commandHandler = std::make_shared<CommandHandler>();
+        parser->countOccurrencesWord.connect(boost::bind(&Invoker::countOccurrencesWord, invoker, _1, _2));
+        parser->calculateChecksum.connect(boost::bind(&Invoker::calculateCheckSum, invoker, _1));
+        invoker->start.connect(boost::bind(&CommandHandler::start, commandHandler, _1));
         parser->startParsing(argc, argv);
     }
     catch (ArgsParseException &e) {
